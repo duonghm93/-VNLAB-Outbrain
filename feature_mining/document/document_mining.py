744813810b40d47b7578e8025e868cd2c551acd2
Duong Hoang-Minh <duonghm@runsystem.net>
2017-10-11 17:22:00 +0700

    fix __get__click__number

--- feature_mining/document/document_mining.py
+++ feature_mining/document/document_mining.py
@@ -6,11 +6,10 @@ class DocumentMining:
     def __init__(self, df_merge_train, df_pageviews):
         self.df_pageviews = df_pageviews
         self.df_merge_train = df_merge_train
-        self.total_pageviews = df_pageviews.shape[0]
+        self.total_pageviews = None
         self.total_click = None
 
     def get_total_click(self):
-        # TODO Need to calculate total click
         if self.total_click is None:
             self.total_click = self.df_merge_train[constant.CLICKED_COLUMN_NAME].sum()
         return self.total_click
@@ -22,7 +21,7 @@ class DocumentMining:
 
     # Support
     def __get__click__number(self, __id, __field__name):
-        return self.df_merge_train[(self.df_merge_train[__field__name] == id) & (self.df_merge_train[constant.CLICKED_COLUMN_NAME] == 1)].shape[0]
+        return self.df_merge_train[(self.df_merge_train[__field__name] == __id) & (self.df_merge_train[constant.CLICKED_COLUMN_NAME] == 1)].shape[0]
 
     def __get__appear__number(self, __id, __field__name):
         return self.df_pageviews[self.df_pageviews[__field__name] == __id].shape[0]
@@ -30,12 +29,13 @@ class DocumentMining:
     def __get__feature(self, __id, __field__name):
         number_of_click = self.__get__click__number(__id, __field__name)
         number_of_appear = self.__get__appear__number(__id, __field__name)
+        print(number_of_click, number_of_appear)
         click_rate_over_all_click = number_of_click / self.get_total_click()
         click_rate = 0
         if number_of_appear is not 0:
             click_rate = number_of_click / number_of_appear
         elif number_of_click is not 0:
-            click_rate = 1
+            click_rate = click_rate_over_all_click * constant.DEFAULT_CLICK_RATE_OVER_APPEAR_RATE
         else:
             click_rate = 0
         appear_rate = number_of_appear / self.get_total_pageviews()
@@ -51,10 +51,9 @@ class DocumentMining:
         return self.__get__feature(publisher_id, constant.DOCUMENT_PUBLISHER_ID_COLUMN_NAME)
 
 
-
 if __name__ == '__main__':
     df_merge_train = pd.read_csv('D:/outbrain/sample_data.csv', header=0)
     df_page_view = pd.read_csv(constant.get_page_view_sample_file(), header=0)
     docMining = DocumentMining(df_merge_train, df_page_view)
-    doc_id = 1823412
-    print(docMining.get_doc_id_feature())
+    doc_id = 778157
+    print(docMining.get_doc_id_feature(doc_id))
